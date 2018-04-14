1e4e86836445327d2d4e1a3db3ab177e5e76d4f5
Albert Navarro Perez <37250591+alnape@users.noreply.github.com>
2018-04-14 17:47:06 +0200

    Add files via upload
    Versió 2

--- NBAscraper.py
+++ NBAscraper.py
@@ -2,15 +2,15 @@ class NBA():
     
     def __init__(self):
         self.home_page = "http://www.espn.com"
-        print(self.home_page)
+        #print(self.home_page)
         self.teams_page = "http://www.espn.com/nba/players"
-        print(self.teams_page)
+        #print(self.teams_page)
         self.data = []
-        print(self.data)
+        #print(self.data)
         
 
     def data2csv(self, filename, data):
         # Overwrite to the specified file.
 	    # Create it if does not exist.
         file = open(filename, "w+")
-        data.to_csv(path_or_buf=file, sep=';', chunksize=1000, columns=['NO','NAME','POS','AGE','HEIGHT','WEIGHT','COLLEGE','2017-2018 SALARY'])
+        data.to_csv(path_or_buf=file, sep=';', chunksize=1000, columns=["TEAM", "NO", "NAME", "POS", "AGE", "HEIGHT", "WEIGHT", "COLLEGE", "SALARY"])
--- main.py
+++ main.py
@@ -18,10 +18,19 @@ def getTeamLinks(url_home, url_teams):
     teams = soup.find(id="my-players-table")
 
     links = [] 
+    names = [] 
     for link in teams.findAll('a', attrs={'href': re.compile("roster")}):
-        links.append(url_home + link.get('href'))
- 
-    return links
+        links.append(url_home + link.get('href').strip())        
+        names.append(link.get_text().strip())
+    
+    data = pd.DataFrame({
+        "url_team": links, 
+        "name_team": names,
+        })
+    #print(data)    
+    #print(data['url_team'])    
+    #return links
+    return data
 
 def getPlayersTeam(url_team):
     html_page = requests.get(url_team)
@@ -30,79 +39,64 @@ def getPlayersTeam(url_team):
     links = [] 
     for link in players.findAll('a', attrs={'href': re.compile("player")}):
         #links.append(link.get('href'))
-        links.append(link.get_text())
+        links.append(link.get_text().strip())
     
     return links
 
-def scrap_table(url_team):
+def scrap_table(url_team, name_team):
     print(url_team)
     html_page = requests.get(url_team)
     soup = BeautifulSoup(html_page.content, 'html.parser')
-    #print(soup.prettify())
-    #f = open('workfile', 'w')
-    #f.write(soup.prettify())
-    # Only oddrow and evenrow
-    lista = []
-    for table_row in soup.select('tablehead tr[class*="row"]'):
-    #for table_row in soup.select('tablehead tr'):        
-        lista.append(table_row)
-        print(table_row) #.get_text())
-    return lista
+    #soup = BeautifulSoup(open("C:\\table.html"), "html.parser")
+    lst_players = []
+    # Table that contains players
+    tbl_players = soup.find_all('tr', {'class': re.compile('player')})
 
-#team_links = getTeamLinks(home_page, teams_page)
-#print(team_links[0])
-#players_team = getPlayersTeam(team_links[0])
-#print(players_team)
+    for player in tbl_players:
+        lst_players.append(player)
 
-#mytable = scrap_table(team_links[0])
-#mytable = scrap_table(team_links[0])
-soup = BeautifulSoup(open("C:\\table.html"), "html.parser")
-#print(soup.prettify())
-lst_players = []
-# Table that contains players
-tbl_players = soup.find_all('tr', {'class': re.compile('player')})
+    #Attributes: TEAM, NO., NAME, POSITION, AGE, HEIGHT, WEIGHT, COLLEGE, 2017-2018 SALARY
+    lst_team = []
+    lst_nos = []
+    lst_names = []
+    lst_pos = []
+    lst_ages = []    
+    lst_hts = []    
+    lst_wts = []    
+    lst_cols = []
+    lst_sals = []
+    i = 0    
+    while (i < len(lst_players)):    
+        #TEAM
+        lst_team.append(name_team)
+        #NO.    
+        attr_no = lst_players[i].find_all('td')[0].get_text().strip()  
+        lst_nos.append(attr_no)  
+        #NAME
+        attr_name = lst_players[i].find_all('td')[1].get_text().strip() 
+        lst_names.append(attr_name)  
+        #POS
+        attr_pos = lst_players[i].find_all('td')[2].get_text().strip()
+        lst_pos.append(attr_pos)
+        #AGE
+        attr_age = lst_players[i].find_all('td')[3].get_text().strip()
+        lst_ages.append(attr_age)
+        #HT
+        attr_ht = lst_players[i].find_all('td')[4].get_text().strip()
+        lst_hts.append(attr_ht)
+        #WT
+        attr_wt = lst_players[i].find_all('td')[5].get_text().strip()
+        lst_wts.append(attr_wt)
+        #COLLEGE
+        attr_col = lst_players[i].find_all('td')[6].get_text().strip()
+        lst_cols.append(attr_col)
+        #2017-2018 SALARY
+        attr_sal = lst_players[i].find_all('td')[7].get_text().strip()
+        lst_sals.append(attr_sal)
+        i = i + 1
 
-for player in tbl_players:
-    lst_players.append(player)
-
-#Attributes: NO., NAME, POSITION, AGE, HEIGHT, WEIGHT, COLLEGE, 2017-2018 SALARY
-lst_nos = []
-lst_names = []
-lst_pos = []
-lst_ages = []    
-lst_hts = []    
-lst_wts = []    
-lst_cols = []
-lst_sals = []
-i = 0    
-while (i < len(lst_players)):    
-    #NO.    
-    attr_no = lst_players[i].find_all('td')[0].get_text().strip()  
-    lst_nos.append(attr_no)  
-    #NAME
-    attr_name = lst_players[i].find_all('td')[1].get_text().strip() 
-    lst_names.append(attr_name)  
-    #POS
-    attr_pos = lst_players[i].find_all('td')[2].get_text().strip()
-    lst_pos.append(attr_pos)
-    #AGE
-    attr_age = lst_players[i].find_all('td')[3].get_text().strip()
-    lst_ages.append(attr_age)
-    #HT
-    attr_ht = lst_players[i].find_all('td')[4].get_text().strip()
-    lst_hts.append(attr_ht)
-    #WT
-    attr_wt = lst_players[i].find_all('td')[5].get_text().strip()
-    lst_wts.append(attr_wt)
-    #COLLEGE
-    attr_col = lst_players[i].find_all('td')[6].get_text().strip()
-    lst_cols.append(attr_col)
-    #2017-2018 SALARY
-    attr_sal = lst_players[i].find_all('td')[7].get_text().strip()
-    lst_sals.append(attr_sal)
-    i = i + 1
-
-data = pd.DataFrame({
+    data = pd.DataFrame({
+        "TEAM": lst_team,
         "NO": lst_nos, 
         "NAME": lst_names, 
         "POS": lst_pos, 
@@ -110,12 +104,41 @@ data = pd.DataFrame({
         "HEIGHT":lst_hts,
         "WEIGHT":lst_wts,
         "COLLEGE":lst_cols,
-        "2017-2018 SALARY":lst_sals
+        "SALARY":lst_sals
     })
-print(data)
+    #print(data)
+    return data
+    #print(data)
+
+team_data = getTeamLinks(home_page, teams_page)
+all_data = pd.DataFrame(columns=["TEAM", "NO", "NAME", "POS", "AGE", "HEIGHT", "WEIGHT", "COLLEGE", "SALARY"])
+#all_data.loc[0] = ["First",0,".",".",0,".",0,".","."]
+all_data1 = pd.DataFrame(columns=["TEAM", "NO", "NAME", "POS", "AGE", "HEIGHT", "WEIGHT", "COLLEGE", "SALARY"])
+all_data1.loc[0] = ["Second",0,".",".",0,".",0,".","."]
+#frames = [all_data, all_data1]
+print(pd.concat([all_data, all_data1]))
+#print(all_data1)
+
+for i, row in enumerate(team_data.itertuples(), 1):
+    print("Scraping data: "+ row.url_team)
+    current_data = scrap_table(row.url_team, row.name_team)
+    all_data = pd.concat([all_data, current_data])
+    '''
+    if i==1:
+        print("Scraping data: "+ row.name_team)
+        current_data = scrap_table(row.url_team, row.name_team)
+    elif i==2:
+        current_data = scrap_table(row.url_team, row.name_team)
+        #all_data.append(current_data)
+        all_data = pd.concat([all_data, current_data])
+    '''
+#print(all_data)
+    #all_data.append(team_data)
+
+#players_team = getPlayersTeam(team_links[0])
 
 output_file = "dataset.csv"
-scraper.data2csv(output_file, data)
+scraper.data2csv(output_file, all_data)
 
 #self.data.append(lst_names)
 #scraper.scrape();
